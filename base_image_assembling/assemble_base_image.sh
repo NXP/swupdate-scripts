1758bc51946fd5f4cde669d72a81fa658888602b
Terry Lv <Terry.Lv@nxp.com>
2022-09-09 01:20:16 +0800

    Support partition image program directly.
    - Support partition image program directly.
    - Add imx6ullwch board support.
    
    Signed-off-by: Terry Lv <Terry.Lv@nxp.com>

--- base_image_assembling/assemble_base_image.sh
+++ base_image_assembling/assemble_base_image.sh
@@ -13,14 +13,6 @@ function print_help()
 }
 
 WRK_DIR="$(pwd)"
-#CUR_DATE=$(date "+%Y%m%d-%H%M%S")
-CUR_DATE=$(date "+%Y%m%d")
-
-SUPPORTED_SOC="
-imx8mm
-imx6ull
-"
-
 TMP_BIN_FILE="./tmp.bin"
 
 OUTPUT_IMAGE_NAME=""
@@ -61,24 +53,13 @@ while getopts "o:deb:mh" arg; do
 	esac
 done
 
-if [ -z "${SOC}" ]; then
-	echo "No SOC specified!"
-	exit 1
-else
-	VALID_SOC_FLAG=false
-	for each_item in $SUPPORTED_SOC; do
-		if [ x${each_item} == x${SOC} ]; then
-			VALID_SOC_FLAG=true
-		fi
-	done
-	if [ x${VALID_SOC_FLAG} == x"${false}" ]; then
-		echo "Not supported SoC: ${SOC}"
-	fi
-fi
+source ${WRK_DIR}/../utils/utils.sh
+source ${WRK_DIR}/../boards/cfg_boards.cfg
+
+check_valid_boards $SOC
 
 SOC_ASSEMBLE_SETTING_FILE="cfg_${SOC}_base.cfg"
 source ${WRK_DIR}/../boards/${SOC_ASSEMBLE_SETTING_FILE}
-source ${WRK_DIR}/../utils/utils.sh
 
 if [ -z "${OUTPUT_IMAGE_NAME}" ]; then
 	echo "No output image name specified! Use default name!"
@@ -91,27 +72,27 @@ if test -e ./$OUTPUT_IMAGE_NAME; then
 	echo "DONE"
 fi
 
-echo -n ">>>> Check MBR file..."
-IMAGE_MBR_SIZE=$(echo ${IMAGE_MBR} | cut -d: -f3)
-MBR_FILENAME=$(basename ${IMAGE_MBR_PATH})
-MBR_FILEDIR=$(dirname ${IMAGE_MBR_PATH})
-if [ x$GENERATE_MBR_ONLY_FLAG == xtrue ]; then
-	echo ">>>> Regenerate or overwrite MBR..."
-	if [ -e ${IMAGE_MBR_PATH} ]; then
-		echo "Delete existed MBR ${IMAGE_MBR_PATH}"
-		rm ${IMAGE_MBR_PATH}
+echo -n ">>>> Check partition table file..."
+IMAGE_PT_TBL_SIZE=$(echo ${IMAGE_PT_TBL} | cut -d: -f3)
+PT_FILENAME=$(basename ${IMAGE_PT_TBL_PATH})
+PT_FILEDIR=$(dirname ${IMAGE_PT_TBL_PATH})
+if [ x$GENERATE_PT_TBL_ONLY_FLAG == xtrue ]; then
+	echo ">>>> Regenerate or overwrite partition table..."
+	if [ -e ${IMAGE_PT_TBL_PATH} ]; then
+		echo "Delete existed partition table ${IMAGE_PT_TBL_PATH}"
+		rm ${IMAGE_PT_TBL_PATH}
 	fi
-	echo "Generating MBR ${IMAGE_MBR_PATH}"
-	cd $MBR_FILEDIR
-	generate_mbr_dualslot $MBR_FILENAME $IMAGE_MBR_SIZE
+	echo "Generating ${IMAGE_PT_TBL_PATH}"
+	cd $PT_FILEDIR
+	generate_pt_tbl_dualslot $PT_FILENAME $IMAGE_PT_SIZE $IMAGE_PT_TBL_FMT
 	cd -
 	echo "DONE"
 	exit 0
 else
-	if [ ! -e ${IMAGE_MBR_PATH} ]; then
-		echo -n "\nNo MBR file, will generate MBR..."
-		cd $MBR_FILEDIR
-		generate_mbr_dualslot $MBR_FILENAME $IMAGE_MBR_SIZE
+	if [ ! -e ${IMAGE_PT_TBL_PATH} ]; then
+		echo -n "\nNo partition table file, will generate ..."
+		cd $PT_FILEDIR
+		generate_pt_tbl_dualslot $PT_FILENAME $IMAGE_PT_TBL_SIZE $IMAGE_PT_TBL_FMT
 		cd -
 	fi
 fi
@@ -119,36 +100,44 @@ echo "DONE"
 
 echo -n ">>>> Check slota boot partition mirror..."
 BOOT_PT=$(echo $SLOTA_BOOT_PT | cut -d: -f1)
-if [ ! -e ${BOOT_PT} ]; then
-	echo ""
-	echo -n "No slata boot partition mirror, generate..."
-	cd $BOOT_PT_DIR
-	calculate_pt_size $SLOTA_BOOT_PT PT_SIZE
-	truncate -s ${PT_SIZE} ${BOOT_PT}
-	cd -
+if [ -z ${BOOT_PT} ]; then
+	echo "No slotA boot part, ignored!"
+else
+	if [ ! -e ${BOOT_PT} ]; then
+		echo ""
+		echo -n "No slota boot partition mirror, generate..."
+		cd $BOOT_PT_DIR
+		calculate_pt_size $SLOTA_BOOT_PT PT_SIZE
+		truncate -s ${PT_SIZE} ${BOOT_PT}
+		cd -
+	fi
 fi
 echo "DONE"
 
 echo -n ">>>> Check slotb boot partition mirror..."
 BOOT_PT=$(echo $SLOTB_BOOT_PT | cut -d: -f1)
-if [ ! -e ${BOOT_PT} ]; then
-	echo ""
-	echo -n "\nNo slatb boot partition mirror, generate..."
-	calculate_pt_size $SLOTB_BOOT_PT PT_SIZE
-	truncate -s ${PT_SIZE} ${BOOT_PT}
+if [ -z ${BOOT_PT} ]; then
+	echo "No slotB boot part, ignored!"
+else
+	if [ ! -e ${BOOT_PT} ]; then
+		echo ""
+		echo -n "\nNo slotb boot partition mirror, generate..."
+		calculate_pt_size $SLOTB_BOOT_PT PT_SIZE
+		truncate -s ${PT_SIZE} ${BOOT_PT}
+	fi
 fi
 echo "DONE"
 
 echo -n ">>>> Check slota link..."
 if test ! -d ${WRK_DIR}/slota; then
-	echo "ERROR: need to link slata to yocto image deploy directory!"
+	echo "ERROR: need to link slota to yocto image deploy directory!"
 	exit 1
 fi
 echo "DONE"
 
 echo -n ">>>> Check slotb link..."
 if test ! -d ${WRK_DIR}/slotb; then
-	echo "ERROR: need to link slatb to yocto image deploy directory!"
+	echo "ERROR: need to link slotb to yocto image deploy directory!"
 	exit 1
 fi
 echo "DONE"
@@ -158,77 +147,159 @@ touch $OUTPUT_IMAGE_NAME
 
 # 1. assemble header
 echo ">>>> Making header..."
-for each_item in $IMAGES_HEADER; do
-	img_file=$(echo $each_item | cut -d: -f1)
-	pad_start=$(echo $each_item | cut -d: -f2)
-	pad_end=$(echo $each_item | cut -d: -f3)
-	touch $TMP_BIN_FILE
-	generate_padding_file $pad_start $img_file $pad_end $TMP_BIN_FILE
-	if [ $? != 0 ]; then
+if [ -z $IMAGES_HEADER ]; then
+	echo "NULL header, ignored!"
+else
+	for each_item in $IMAGES_HEADER; do
+		img_file=$(echo $each_item | cut -d: -f1)
+		pad_start=$(echo $each_item | cut -d: -f2)
+		pad_end=$(echo $each_item | cut -d: -f3)
+		touch $TMP_BIN_FILE
+		generate_padding_file $pad_start $img_file $pad_end $TMP_BIN_FILE
+		if [ $? != 0 ]; then
+			rm -f $TMP_BIN_FILE
+			exit 1
+		fi
+		cat $TMP_BIN_FILE >> $OUTPUT_IMAGE_NAME
 		rm -f $TMP_BIN_FILE
-		exit 1
-	fi
-	cat $TMP_BIN_FILE >> $OUTPUT_IMAGE_NAME
-	rm -f $TMP_BIN_FILE
-done
+	done
+fi
 echo "DONE"
 
 # 2. assemble swupdate
 echo ">>>> Making swupdate..."
-for each_item in $IMAGES_SWUPDATE; do
-	img_file=$(echo $each_item | cut -d: -f1)
-	pad_start=$(echo $each_item | cut -d: -f2)
-	pad_end=$(echo $each_item | cut -d: -f3)
-	touch $TMP_BIN_FILE
-	generate_padding_file $pad_start $img_file $pad_end $TMP_BIN_FILE
-	if [ $? != 0 ]; then
+if [ -z $IMAGES_SWUPDATE ]; then
+	echo "NULL swupdate ramdisk images, Ignored!"
+else
+	for each_item in $IMAGES_SWUPDATE; do
+		img_file=$(echo $each_item | cut -d: -f1)
+		pad_start=$(echo $each_item | cut -d: -f2)
+		pad_end=$(echo $each_item | cut -d: -f3)
+		touch $TMP_BIN_FILE
+		generate_padding_file $pad_start $img_file $pad_end $TMP_BIN_FILE
+		if [ $? != 0 ]; then
+			rm -f $TMP_BIN_FILE
+			exit 1
+		fi
+		cat $TMP_BIN_FILE >> $OUTPUT_IMAGE_NAME
 		rm -f $TMP_BIN_FILE
-		exit 1
-	fi
-	cat $TMP_BIN_FILE >> $OUTPUT_IMAGE_NAME
-	rm -f $TMP_BIN_FILE
-done
+	done
+fi
 echo "DONE"
 
 # 3. assemble slota
 echo ">>>> Making slota..."
-BOOT_PT_PATH=$(echo $SLOTA_BOOT_PT | cut -d: -f1)
-copy_images_to_boot_pt $BOOT_PT_PATH slota
-ROOTFS_IMG=$(echo $SLOTA_ROOTFS | cut -d: -f1)
-if [ ! -e $ROOTFS_IMG ]; then
-	echo "SLOTA ROOTFS image not found!"
-	exit -1
+if [ -z $SLOTA_BOOT_PT_FILES ]; then
+	echo "No slotA files to copy to boot part, ignored!"
+else
+	BOOT_PT_PATH=$(echo $SLOTA_BOOT_PT | cut -d: -f1)
+	copy_images_to_boot_pt $BOOT_PT_PATH slota
 fi
-calculate_pt_size $SLOTA_ROOTFS PT_SIZE
-truncate -s $PT_SIZE $ROOTFS_IMG
-e2fsck -f $ROOTFS_IMG
-resize2fs $ROOTFS_IMG
-for each_item in $SLOTA_IMAGES; do
-	item_path=$(echo $each_item | cut -d: -f1)
-	cat $item_path >> $OUTPUT_IMAGE_NAME
-done
+if [ -z $SLOTA_ROOTFS ]; then
+	echo "No slotA rootfs, ignored!"
+else
+	ROOTFS_IMG=$(echo $SLOTA_ROOTFS | cut -d: -f1)
+	if [ ! -e $ROOTFS_IMG ]; then
+		echo "SLOTA ROOTFS image not found!"
+		exit -1
+	fi
+	calculate_pt_size $SLOTA_ROOTFS PT_SIZE
+	truncate -s $PT_SIZE $ROOTFS_IMG
+	e2fsck -f $ROOTFS_IMG
+	resize2fs $ROOTFS_IMG
+	for each_item in $SLOTA_IMAGES; do
+		item_path=$(echo $each_item | cut -d: -f1)
+		cat $item_path >> $OUTPUT_IMAGE_NAME
+	done
+fi
 echo "DONE"
 
 # 4. assemble slotb
 if [ x$DOUBLESLOT_FLAG == x"true" ]; then
 	echo ">>>> Making slotb..."
-	BOOT_PT_PATH=$(echo $SLOTB_BOOT_PT | cut -d: -f1)
-	copy_images_to_boot_pt $BOOT_PT_PATH slotb
-	ROOTFS_IMG=$(echo $SLOTB_ROOTFS | cut -d: -f1)
-	if [ ! -e $ROOTFS_IMG ]; then
-		echo "SLOTB ROOTFS image not found!"
-		exit -1
+	if [ -z $SLOTA_BOOT_PT_FILES ]; then
+		echo "No slotB files to copy to boot part, ignored!"
+	else
+		BOOT_PT_PATH=$(echo $SLOTB_BOOT_PT | cut -d: -f1)
+		copy_images_to_boot_pt $BOOT_PT_PATH slotb
 	fi
-	calculate_pt_size $SLOTB_ROOTFS PT_SIZE
-	truncate -s $PT_SIZE $ROOTFS_IMG
-	e2fsck -f $ROOTFS_IMG
-	resize2fs $ROOTFS_IMG
-	for each_item in $SLOTB_IMAGES; do
-		item_path=$(echo $each_item | cut -d: -f1)
-		cat $item_path >> $OUTPUT_IMAGE_NAME
-	done
-	echo "DONE"
+	if [ -z $SLOTB_ROOTFS ]; then
+		echo "No slotB rootfs, ignored!"
+	else
+		ROOTFS_IMG=$(echo $SLOTB_ROOTFS | cut -d: -f1)
+		if [ ! -e $ROOTFS_IMG ]; then
+			echo "SLOTB ROOTFS image not found!"
+			exit -1
+		fi
+		calculate_pt_size $SLOTB_ROOTFS PT_SIZE
+		truncate -s $PT_SIZE $ROOTFS_IMG
+		e2fsck -f $ROOTFS_IMG
+		resize2fs $ROOTFS_IMG
+		for each_item in $SLOTB_IMAGES; do
+			item_path=$(echo $each_item | cut -d: -f1)
+			cat $item_path >> $OUTPUT_IMAGE_NAME
+		done
+		echo "DONE"
+	fi
 fi
+
+# 5. Check image map
+echo "Checking partition image ..."
+if [ -z $IMAGE_PT_TBL_IMAGES ]; then
+	echo "No extra image map found, ignored!"
+else
+	echo "Find partition images, will program these images to disk partitions!"
+	# Validate images
+	for each_item in $IMAGE_PT_TBL_IMAGES; do
+		pt_image_path=$(echo $each_item | cut -d: -f2)
+		pt_image_offset=$(echo $each_item | cut -d: -f3)
+		if [ ! -e $pt_image_path ]; then
+			echo "Can't find $pt_image_path to dd!"
+			exit -1
+		fi
+		if [ -z $pt_image_offset ]; then
+			echo "Offset not found for $pt_image_path!"
+			exit -1
+		fi
+	done
+	cp $IMAGE_PT_TBL_PATH $OUTPUT_IMAGE_NAME
+	for each_item in $IMAGE_PT_TBL_IMAGES; do
+		pt_img_path=$(echo $each_item | cut -d: -f2)
+		pad_start=$(echo $each_item | cut -d: -f3)
+		pad_end=$(echo $each_item | cut -d: -f4)
+		touch $TMP_BIN_FILE
+		generate_padding_file $pad_start $img_file $pad_end $TMP_BIN_FILE
+		if [ $? != 0 ]; then
+			rm -f $TMP_BIN_FILE
+			exit 1
+		fi
+		cat $TMP_BIN_FILE >> $OUTPUT_IMAGE_NAME
+		rm -f $TMP_BIN_FILE
+	done
+fi
+echo "DONE"
+
+# 6. Check image tail for GPT
+echo "Checking image tail for GPT..."
+if [ x$IMAGE_PT_TBL_FMT == x"GPT" ]; then
+	if [ -z $IMAGES_TAIL ]; then
+		echo "Error: no image tail found!"
+		exit -1
+	else
+		for each_item in $IMAGES_TAIL; do
+			pt_img_path=$(echo $each_item | cut -d: -f1)
+			pad_start=$(echo $each_item | cut -d: -f2)
+			pad_end=$(echo $each_item | cut -d: -f3)
+			touch $TMP_BIN_FILE
+			generate_padding_file $pad_start $img_file $pad_end $TMP_BIN_FILE
+			cat $TMP_BIN_FILE >> $OUTPUT_IMAGE_NAME
+			rm -f $TMP_BIN_FILE
+		done
+	fi
+else
+	echo "Not using GPT, ignored!"
+fi
+echo "DONE"
 
 echo "==========================================================================="
 echo "Create base image $OUTPUT_IMAGE_NAME successfully"
