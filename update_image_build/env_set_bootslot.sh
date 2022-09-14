ffebb81fd480bd8c05716a11ec35d81563cd2140
Terry Lv <Terry.Lv@nxp.com>
2022-09-14 17:35:38 +0800

    Change env command parameters.
    - Change env command parameters.
    
    Signed-off-by: Terry Lv <Terry.Lv@nxp.com>

--- update_image_build/env_set_bootslot.sh
+++ update_image_build/env_set_bootslot.sh
@@ -1,12 +1,13 @@
+echo "Set upgrade_available to 1"
+hub_setenv --upgrade_available 1
+
 echo "Switch boot_slot to... " 
 cur_boot_slot=$(hub_printenv -f boot_slot)
-if [ x$cur_boot_slot == x"1" ]; then
+if [ x$cur_boot_slot == x"A" ]; then
 	echo "SlotB"
-	hub_setenv boot_slot 2
+	hub_setenv --boot_slot 2
 else
 	echo "SlotA"
-	hub_setenv boot_slot 1
+	hub_setenv --boot_slot 1
 fi
-hub_setenv upgrade_available 1
-echo "Set upgrade_available to 1"
-hub_setenv upgrade_available 1
+
