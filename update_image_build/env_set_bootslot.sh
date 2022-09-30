483ee0a485b2269d3e620abdaacb0c5aa59e6abf
Terry Lv <Terry.Lv@nxp.com>
2022-09-30 15:23:07 +0800

    Fix string manipulate error in lua.
    - Fix string manipulate error in lua.
    
    Signed-off-by: Terry Lv <Terry.Lv@nxp.com>

--- update_image_build/env_set_bootslot.sh
+++ update_image_build/env_set_bootslot.sh
@@ -6,10 +6,10 @@ hub_setenv --upgrade_available 1
 echo "Switch boot_slot to... " 
 cur_boot_slot=$(hub_printenv -f boot_slot)
 if [ x$cur_boot_slot == x"A" ]; then
-	echo "SlotB"
+	echo "Switch to SlotB"
 	hub_setenv --boot_slot 2
 else
-	echo "SlotA"
+	echo "Switch to SlotA"
 	hub_setenv --boot_slot 1
 fi
 
